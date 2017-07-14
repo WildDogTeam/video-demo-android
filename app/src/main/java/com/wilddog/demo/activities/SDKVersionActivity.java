076880382860e0d1069649c8c28ff10cf425d998
DongXiaofei <dongxiaofei@wilddog.com>
2017-07-14 19:08:22 +0800

    chagnge ui

--- app/src/main/java/com/wilddog/demo/activities/SDKVersionActivity.java
+++ app/src/main/java/com/wilddog/demo/activities/SDKVersionActivity.java
@@ -12,6 +12,7 @@ public class SDKVersionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //TODO 手动修改版本
         setContentView(R.layout.activity_sdk_version);
         ivCancel = (ImageView) findViewById(R.id.iv_cancel);
         ivCancel.setOnClickListener(new View.OnClickListener() {
